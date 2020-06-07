ff1fe3ef9e833a8d5f053e6727d73e939a2ff10b
Andrew Chen <andrewchen@Andrews-MacBook-Pro-3.local>
2020-06-06 22:31:17 -0700

    changes

--- java/src/Ticketmaster.java
+++ java/src/Ticketmaster.java
@@ -23,6 +23,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * This class defines a simple embedded SQL utility class that is designed to
@@ -312,8 +313,22 @@ public class Ticketmaster{
 		return input;
 	}//end readChoice
 	
+	//Ticketmaster esql is a java object
+
 	public static void AddUser(Ticketmaster esql){//1
+		// insert tuple into database
+		try {
+			do {
+				
+			
+			} while (true) {
+
+			}
 		
+		
+		} catch (Exception e) {
+
+		} 
 	}
 	
 	public static void AddBooking(Ticketmaster esql){//2
