9945256f0fc1cbda98904f5aca1da22b72a234e2
Andrew Chen <andrewrchen05@gmail.com>
2020-06-10 17:22:03 -0700

    attempt to fix quotations

--- java/src/Ticketmaster.java
+++ java/src/Ticketmaster.java
@@ -409,7 +409,8 @@ public class Ticketmaster{
 
 		//insert into table
 		try {
-			String query = "INSERT INTO Users (email, lname, fname, phone, pwd) VALUES (" + email + ", '" + lname + "', '" + fname + ", '" + phone + ", '" + pwd + ");";
+			String query = "INSERT INTO Users (email, lname, fname, phone, pwd) VALUES ('" + email + "', '" + lname + "', '" + fname + "', '" + phone + "', '" + pwd + "');";
+			
 			esql.executeUpdate(query);
 		} catch(Exception e) {
 			System.out.println(e.getMessage());
