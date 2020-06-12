63cc7a2013fdd48509393145026b829ec2cf61a5
Andrew Chen <andrewrchen05@gmail.com>
2020-06-12 01:35:36 -0700

    fix 10

--- java/src/Ticketmaster.java
+++ java/src/Ticketmaster.java
@@ -1008,33 +1008,32 @@ public class Ticketmaster{
 	}
 	
 	public static void ListShowsStartingOnTimeAndDate(Ticketmaster esql){//10
-		String sdate;
-		String sttime;
 
+		String date = "";
+		String time = "";
+		
 		do{
-			System.out.println("Show date(M/D/YYYY): ");
 			try {
-				sdate = in.readLine();
-				break;
+				System.out.println("Which day does the customer want to attend the show?: ");
+				date = in.readLine();
+				System.out.println("What time does the customer want to attend the show?: ");
+				time = in.readLine();
+				if((date.length() > 10 || date.length() == 0) || (time.length() > 8 || time.length() == 0))  {
+					throw new RuntimeException("Date cannot be more than 10 characters and time cannot be more than 8 characters");
+				}
+				else {
+					break;
+				}
+
 			} catch(Exception e) {
 				System.out.println("Your input is invalid!");
-					continue;
-			}
-		} while(true);
-
-		do{
-			System.out.println("Start time in military time(HH:MM): ");
-			try {
-				sttime = in.readLine();
-				break;
-			} catch(Exception e) {
-				System.out.println("Your input is invalid!");
-					continue;
+				System.exit(0); // we are not implementing sophisticated error checking
+				continue;
 			}
 		} while(true);
 
 		try {
-			String query = "SELECT S.sid FROM Shows S Movies M WHERE S.sdate = " + sdate + " AND S.sttime = " + sttime + " AND M.mvid = S.mvid;";
+			String query = "SELECT S.sid, M.title FROM Shows S Movies M WHERE S.sdate = " + date + " AND S.sttime = " + ttime + " AND M.mvid = S.mvid;";
 			esql.executeQueryAndPrintResult(query);
 		} catch(Exception e) {
 			System.out.println(e.getMessage());
